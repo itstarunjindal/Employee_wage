dc9f318e60068ccd9aa92de98f0cf1975be71334
Tarun Jindal <101278379+itstarunjindal@users.noreply.github.com>
2024-12-04 13:55:52 +0530

    uc1

---
+++ Employee.rb
@@ -0,0 +1,11 @@
+
+def attendance(remarks)
+  if(remarks == 1)
+    puts "Employee is present"
+  else
+    puts "Employee is absent"
+  end
+end
+
+Employee_attendance = rand(0..1)
+attendance(Employee_attendance)
