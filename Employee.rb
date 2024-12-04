8d0b921419f460cf7c583522e0b04fca4e2f01f9
Tarun Jindal <101278379+itstarunjindal@users.noreply.github.com>
2024-12-04 14:19:40 +0530

    UC1

--- Employee.rb
+++ Employee.rb
@@ -1,11 +1,11 @@
+      ## uc1 ##
+# def attendance(remarks)
+#   if(remarks == 1)
+#     puts "Employee is present"
+#   else
+#     puts "Employee is absent"
+#   end
+# end
 
-def attendance(remarks)
-  if(remarks == 1)
-    puts "Employee is present"
-  else
-    puts "Employee is absent"
-  end
-end
-
-Employee_attendance = rand(0..1)
-attendance(Employee_attendance)
+# Employee_attendance = rand(0..1)
+# attendance(Employee_attendance)
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Employee_wage
+
+## uc1 ##
+checking where a employee is present or not.
+
+## uc2 ##
+calculating the wage of an employee.
